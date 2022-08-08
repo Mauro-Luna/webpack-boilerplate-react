ab62af8ee8680af6e842542bd5aa1d60e1aa4720
Mauro Luna <mauroluna.dev@gmail.com>
2022-08-07 21:37:55 -0300

    testin husky 1

--- src/modules/app.js
+++ src/modules/app.js
@@ -6,7 +6,7 @@ function App() {
     <div className={styles.app}>
       <h1>Webpack boilerplate--react</h1>
       <ul>
-        <li>fonts support: woff, woff2, ttf, otf, eot</li>
+         <li>fonts support: woff, woff2, ttf, otf, eot</li>
         <li>media support: png, jpeg, jpg, gif, svg</li>
         <li>postcss support: in dev and prod</li>
          <li>postcss support: Eslint with airbnb</li>
